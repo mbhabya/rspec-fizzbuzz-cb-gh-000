6fb726d01d4a99e9a44b56238c6ce2b3884f207e
Bhabya  Mishra <mbhabya@gmail.com>
2017-09-16 19:48:48 +0000

    automatically backed up by learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,5 @@
 require_relative './spec_helper.rb'
-# require_relative '../fizzbuzz.rb'
+require_relative '../fizzbuzz.rb'
 
 
 describe "fizzbuzz" do
@@ -24,3 +24,4 @@ describe "fizzbuzz" do
     expect(fizz_4).to eq(nil)
   end
 end
+#naho 
