12bfbb364489ba486bd41863b110915c918b50c4
Bhabya  Mishra <mbhabya@gmail.com>
2017-09-16 19:37:21 +0000

    automatically backed up by learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -11,7 +11,7 @@ describe "fizzbuzz" do
     fizz_5 = fizzbuzz(5)
 
     expect(fizz_5).to eq("Buzz")
-  end 
+  end
 end
   it 'returns "FizzBuzz" when the number is divisible by 3 and 5' do
     fizz_15 = fizzbuzz(15)
