2aa20a650d93341e36123eea2a035da7fecf564b
Bhabya  Mishra <mbhabya@gmail.com>
2017-09-16 19:48:28 +0000

    automatically backed up by learn

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,12 @@
 def fizzbuzz
-  # if int % 3 == 0
-  #   "Fizz"
-  # elsif int % 5 == 0
-  #   "Buzz"
-  # end
+  if int % 3 == 0 && int % 5 == 0
+    return "FizzBuzz"
+  end
+  if int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
+  else
+    "nil"
+  end  
 end
